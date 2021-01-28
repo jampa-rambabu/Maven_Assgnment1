34a2a970244b1df49ec477874d2266bca09ecd15
jampa-rambabu <jampa.rambabu8@gmail.com>
2021-01-29 00:56:19 +0530

    created new maven project

---
+++ jampa/src/main/java/jampa/rambabu/mavenpro/jampa/HelloWorld.java
@@ -0,0 +1,9 @@
+package jampa.rambabu.mavenpro.jampa;
+
+public class HelloWorld {
+
+	public static void main ( String[] args) {
+		
+		System.out.println("Hellow Raambabu Your TomCat server Success-fully Deployed");
+	}
+}
